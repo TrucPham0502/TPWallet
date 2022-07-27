d381b9d4420ced14402b3531c5289e359a7d6ec1
Truc Pham <trucpham.work@gmail.com>
2022-07-27 21:22:34 +0700

    Initial Commit

---
+++ TPWallet/TPWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  TPWalletApp.swift
+//  TPWallet
+//
+//  Created by Truc Pham on 27/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct TPWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
